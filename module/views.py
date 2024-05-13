647bf1ccb3fc1b709684551db17ae45087c79d22
bilalobe <elkhatabibilal@gmail.com>
2024-05-13 23:02:29 +0100

    3rd

--- module/views.py
+++ module/views.py
@@ -9,3 +9,12 @@ class ModuleViewSet(viewsets.ModelViewSet):
 class ModuleDetailViewSet(viewsets.ModelViewSet):
     queryset = Module.objects.all()
     serializer_class = ModuleDetailSerializer
+
+    def get(self, request, *args, **kwargs):
+        return super().get(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return super().put(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return super().delete(request, *args, **kwargs)
