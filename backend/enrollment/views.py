763b6ae6e5c2aeb6593cf061ce2887a2a195bbe5
bilalobe <elkhatabibilal@gmail.com>
2024-06-03 13:03:09 +0100

    pkg pipe

--- backend/enrollment/views.py
+++ backend/enrollment/views.py
@@ -1,4 +1,6 @@
-from rest_framework import viewsets, permissions
+# backend/enrollment/views.py
+from rest_framework import viewsets, permissions, status
+from rest_framework.response import Response
 from .models import Enrollment
 from .serializers import EnrollmentSerializer
 
@@ -7,14 +9,31 @@ class EnrollmentViewSet(viewsets.ModelViewSet):
     serializer_class = EnrollmentSerializer
     permission_classes = [permissions.IsAuthenticated]
 
+    def get_queryset(self):
+        # Ensure that users can only see their own enrollments
+        return self.queryset.filter(student=self.request.user)
+
     def perform_create(self, serializer):
+        course = serializer.validated_data['course']
+
+        # Check if the user is already enrolled in the course
+        if Enrollment.objects.filter(student=self.request.user, course=course).exists():
+            return Response(
+                {"detail": "You are already enrolled in this course."},
+                status=status.HTTP_400_BAD_REQUEST
+            )
+
         serializer.save(student=self.request.user)
 
-    def get_queryset(self):
-        user = self.request.user
-        if user.is_authenticated:
-            if user.user_type == 'student':
-                return self.queryset.filter(student=user)
-            elif user.user_type == 'teacher':  # or supervisor
-                return self.queryset.filter(course__instructor=user)
-        return self.queryset.none()
+    def destroy(self, request, *args, **kwargs):
+        instance = self.get_object()
+
+        # Check if the user is the student of the enrollment
+        if instance.student != request.user:
+            return Response(
+                {"detail": "You are not authorized to unenroll from this course."},
+                status=status.HTTP_403_FORBIDDEN
+            )
+
+        self.perform_destroy(instance)
+        return Response(status=status.HTTP_204_NO_CONTENT)
