eb1f04ddedbcf36fe5f945680e5ac6ad46d08de0
bilalobe <elkhatabibilal@gmail.com>
2024-06-07 14:16:08 +0100

    chore: Remove unused files and configurations

--- frontend/tailwind.config.mjs
+++
@@ -1,7 +0,0 @@
-export default {
-  content: ['./templates/**/*.html', './src/**/*.js'], // Use `content` key
-  theme: {
-    extend: {}, // You can add customizations here
-  },
-  plugins: [],
-};
