d290ecc7692e9ee31c6a9f66283964a3b49ed4db
bilalobe <elkhatabibilal@gmail.com>
2024-06-10 14:13:40 +0100

    Create cleanup.py

---
+++ backend/courses/management/commands/cleanup.py
@@ -0,0 +1,8 @@
+from django.core.management.base import BaseCommand
+
+class Command(BaseCommand):
+    help = 'Cleans up old data'
+
+    def handle(self, *args, **options):
+        # Your cleanup code here
+        self.stdout.write(self.style.SUCCESS('Successfully cleaned up old data'))
