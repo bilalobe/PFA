d8871745ec62b8813793126e728d35373096a256
bilalobe <elkhatabibilal@gmail.com>
2024-06-10 14:15:05 +0100

    chore: Update import statement in run_waitress.py

--- backend/run_waitress.py
+++ backend/run_waitress.py
@@ -1,5 +1,6 @@
 from waitress import serve
-from dj_ango.wsgi import application
+from eplatform_backend.wsgi import application
 
 if __name__ == '__main__':
     serve(application, host='0.0.0.0', port=8000)
+
