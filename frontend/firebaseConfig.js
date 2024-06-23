d9ebb2ec207f62829b6c69b1adf9ad6731bceab8
bilalobe <elkhatabibilal@gmail.com>
2024-06-23 02:21:49 +0100

    Update config

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -6,23 +6,7 @@ import { getDatabase } from "firebase/database";
 
 
 
-const firebaseConfig = {
 
-    apiKey: "AIzaSyDa2GjYbLELjm5zVUFSZdluNlacOjyL8Pg",
-  
-    authDomain: "eleanor-80b46.firebaseapp.com",
-  
-    projectId: "eleanor-80b46",
-  
-    storageBucket: "eleanor-80b46.appspot.com",
-  
-    messagingSenderId: "489825674554",
-  
-    appId: "1:489825674554:web:92f39da96b7395c1ab9c03",
-  
-    measurementId: "G-YS2FBXE2FJ"
-  
-  };
   
 
 // Initialize Firebase
