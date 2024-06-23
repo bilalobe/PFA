7724d5409f662f98a8196d608843d7dbe5c1eaba
bilalobe <elkhatabibilal@gmail.com>
2024-06-23 19:06:16 +0100

    refactor: Add CollaborativeEditing component for real-time document collaboration

---
+++ frontend/src/components/Resources/CollaborativeEditing.tsx
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+import { db } from '../../../firebaseConfig';
+import { doc, onSnapshot, updateDoc } from 'firebase/firestore';
+
+const CollaborativeDocument = ({ documentId }) => {
+  const [documentContent, setDocumentContent] = useState('');
+
+  useEffect(() => {
+    const unsubscribe = onSnapshot(doc(db, "documents", documentId), (doc) => {
+      setDocumentContent(doc.data()?.content);
+    });
+
+    return () => unsubscribe();
+  }, [documentId]);
+
+  const handleContentChange = async (e) => {
+    const content = e.target.value;
+    setDocumentContent(content);
+    await updateDoc(doc(db, "documents", documentId), { content });
+  };
+
+  return (
+    <textarea value={documentContent} onChange={handleContentChange} />
+  );
+};
+
+export default CollaborativeDocument;
