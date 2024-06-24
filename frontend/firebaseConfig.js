63eede9840855f4d136727520eef909ce30c61e7
bilalobe <elkhatabibilal@gmail.com>
2024-06-24 13:55:41 +0100

    feat: Successfully initialized Firebase Functions

--- frontend/firebaseConfig.js
+++ frontend/firebaseConfig.js
@@ -8,11 +8,17 @@ import { getFunctions } from "firebase/functions";
 import { getMessaging } from "firebase/messaging";
 import { getPerformance } from "firebase/performance";
 
+// Firebase configuration from Next.js environment variables
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+};
 
-
-
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase services
@@ -25,6 +31,4 @@ const functions = getFunctions(app);
 const messaging = getMessaging(app);
 const performance = getPerformance(app);
 
-
-// Export the services for use in other files
-export { auth, db, storage, database, analytics, functions, messaging, performance, app};
+export { auth, db, storage, database, analytics, functions, messaging, performance, app };
