d75367f9314d6f16b5361f128649f4357b10541f
bilalobe <elkhatabibilal@gmail.com>
2024-07-03 16:01:19 +0100

    refactor: Update ActionProvider to use React hooks for state management

--- frontend/src/components/AI/ActionProvider.tsx
+++ frontend/src/components/AI/ActionProvider.tsx
@@ -1,33 +1,28 @@
+import { useState, useCallback } from 'react';
 
-class ActionProvider {
-  createChatBotMessage: any;
-  setState: any;
-  createClientMessage: any;
-  constructor(createChatBotMessage, setStateFunc, createClientMessage) {
-    this.createChatBotMessage = createChatBotMessage;
-    this.setState = setStateFunc;
-    this.createClientMessage = createClientMessage;
-  }
-
-  // Example action: greeting the user
-  greet() {
-    const greetingMessage = this.createChatBotMessage("Hello friend!");
-    this.updateChatbotState(greetingMessage);
-  }
-
-  // Example action: handling unknown commands
-  handleUnknown() {
-    const message = this.createChatBotMessage("I'm not sure what you mean. Can you rephrase?");
-    this.updateChatbotState(message);
-  }
-
-  // Function to update the state of the chatbot conversation
-  updateChatbotState(message) {
-    this.setState(prevState => ({
-      ...prevState,
-      messages: [...prevState.messages, message]
-    }));
-  }
+interface ChatBotMessage {
+  text: string;
+  id?: string;
 }
+
+const ActionProvider = ({ createChatBotMessage, children }) => {
+  const [messages, setMessages] = useState<ChatBotMessage[]>([]);
+
+  const updateChatbotState = useCallback((message: ChatBotMessage) => {
+    setMessages((prevMessages) => [...prevMessages, message]);
+  }, []);
+
+  const greet = useCallback(() => {
+    const greetingMessage = createChatBotMessage("Hello friend!");
+    updateChatbotState(greetingMessage);
+  }, [createChatBotMessage, updateChatbotState]);
+
+  const handleUnknown = useCallback(() => {
+    const message = createChatBotMessage("I'm not sure what you mean. Can you rephrase?");
+    updateChatbotState(message);
+  }, [createChatBotMessage, updateChatbotState]);
+
+  return children({ greet, handleUnknown, messages });
+};
 
 export default ActionProvider;
