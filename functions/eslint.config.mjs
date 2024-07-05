45a65ff9635dd0a451d79a49fd73445a7af1c69e
bilalobe <elkhatabibilal@gmail.com>
2024-07-05 10:06:07 +0100

    refactor: Update ESLint configuration to use recommended rules

--- functions/eslint.config.mjs
+++ functions/eslint.config.mjs
@@ -1,3 +1,18 @@
-export default {
-  extends: ['next', 'next/core-web-vitals'],
-};
+import js from "@eslint/js";
+
+export default [
+  js.configs.recommended,
+  {
+    files: ["**/*.js"],
+    languageOptions: {
+      ecmaVersion: 2021,
+      sourceType: "module"
+    },
+    rules: {
+      "indent": ["error", 2],
+      "linebreak-style": ["error", "unix"],
+      "quotes": ["error", "double"],
+      "semi": ["error", "always"]
+    }
+  }
+];
