a269e18f5bdaed6e737e1745e71fc7c55a6075f5
BILAL ELKHATABI <Bilal.Elkhatabi@emsi-edu.ma>
2025-04-07 20:43:16 +0100

    feat: Update ESLint configuration and enhance TypeScript settings in tsconfig.json

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,33 +1,24 @@
-module.exports = {
-  root: true,
-  env: {
-    es6: true,
-    node: true,
-  },
-  extends: [
-    "eslint:recommended",
-    "plugin:import/errors",
-    "plugin:import/warnings",
-    "plugin:import/typescript",
-    "google",
-    "plugin:@typescript-eslint/recommended",
-  ],
-  parser: "@typescript-eslint/parser",
-  parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
-    sourceType: "module",
-  },
-  ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
-  ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
-  rules: {
-    "quotes": ["error", "double"],
-    "import/no-unresolved": 0,
-    "indent": ["error", 2],
-  },
+export const root = true;
+export const env = {
+  es6: true,
+  node: true,
+};
+
+export const parser = "@typescript-eslint/parser";
+export const parserOptions = {
+  project: ["tsconfig.json", "tsconfig.dev.json"],
+  sourceType: "module",
+};
+export const ignorePatterns = [
+  "/lib/**/*", // Ignore built files.
+  "/generated/**/*", // Ignore generated files.
+];
+export const plugins = [
+  "@typescript-eslint",
+  "import",
+];
+export const rules = {
+  "quotes": ["error", "double"],
+  "import/no-unresolved": 0,
+  "indent": ["error", 2],
 };
